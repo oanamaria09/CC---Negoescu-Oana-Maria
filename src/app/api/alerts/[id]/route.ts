d71b1801e49c698248a8f1ff6052635e599b6ede
oanamaria09 <negoescuoana21@stud.ase.ro>
2025-05-24 11:38:54 +0300

    Update route.ts

--- src/app/api/alerts/[id]/route.ts
+++ src/app/api/alerts/[id]/route.ts
@@ -1,10 +1,11 @@
 import { db } from "@/lib/firebase";
 import { doc, deleteDoc } from "firebase/firestore";
+import { PagesRouteHandlerContext } from "next/dist/server/route-modules/pages/module.compiled";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function DELETE(
   req: NextRequest,
-  context : { params: { id: string } }
+  context : PagesRouteHandlerContext <{id:string}>
 ) {
   const { id } = context.params;
   await deleteDoc(doc(db, "alerts", id));

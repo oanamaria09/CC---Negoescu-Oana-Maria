d0332c7c3cfdee707e91cde61b4d15c3b5cd1b7a
oanamaria09 <negoescuoana21@stud.ase.ro>
2025-05-24 11:32:49 +0300

    fix deploy

--- src/app/api/alerts/[id]/route.ts
+++ src/app/api/alerts/[id]/route.ts
@@ -4,9 +4,9 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function DELETE(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  context : { params: { id: string } }
 ) {
-  const { id } = params;
+  const { id } = context.params;
   await deleteDoc(doc(db, "alerts", id));
   return NextResponse.json({ success: true });
 }

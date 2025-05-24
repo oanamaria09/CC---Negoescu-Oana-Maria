bfb4e2a5407793709b6882c49455689f51fc5b47
oanamaria09 <negoescuoana21@stud.ase.ro>
2025-05-24 11:43:46 +0300

    Update route.ts

--- src/app/api/alerts/[id]/route.ts
+++ src/app/api/alerts/[id]/route.ts
@@ -1,7 +1,7 @@
 import { db } from "@/lib/firebase";
 import { doc, deleteDoc } from "firebase/firestore";
 import { NextRequest, NextResponse } from "next/server";
-
+// @ts-expect-error Ignore params typing issue in App Router
 export async function DELETE(
   req: NextRequest,
   { params }: { params: { id: string } }
